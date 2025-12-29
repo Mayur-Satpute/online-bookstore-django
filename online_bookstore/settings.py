62864e6b7093a25206e540de3c55d733a411d03f
Mayur Satpute <mayursatpute246@gmail.com>
2025-12-30 00:14:43 +0530

    Add Category and Book models with admin support

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Project apps
+    'accounts',
+    'books',
+    'cart',
+    'orders',
 ]
 
 MIDDLEWARE = [
