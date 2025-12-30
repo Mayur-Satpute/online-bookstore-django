bf59eb0ff04f04785d93538ad4f4afef84a0ad00
Mayur Satpute <mayursatpute246@gmail.com>
2025-12-30 23:22:42 +0530

    Configure media settings for book images

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Media files (Uploaded images)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
